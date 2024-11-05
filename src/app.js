12fe3ff275f7392deb4afc75b9b2f0c22847250b
Your Name <your_email@example.com>
2024-11-06 00:43:02 +0530

    init-Database

--- src/app.js
+++ src/app.js
@@ -1,33 +1,36 @@
 const express = require('express')
 const app = express()
+const connectDB = require('./config/database')
+const User = require('./models/user')
 
-app.use('/', (err, req, res, next) => {
-    if (err) {
-        // Log your error
-        res.status(500).send('something went wrong')
-    }
-})
+app.post('/signup', async (req, res) => {
+    const user = new User({
+        firstName: "Sachin",
+        lastName: "Dutt",
+        emailId: "sachin855@gmail.com",
+        password: "sachin123@"
+    })
+try {
+    await user.save()
+    res.send('Data sent successfully')
+}catch {
+    res.status(400).send('something went wrong')
+}
+}
+)
 
-app.get('/getuserData', (req, res) => {
-    try {
+connectDB().then(() => {
+    console.log('connect to DB');
+    app.listen(3001, () => {
+        console.log("server is running at port 3001");
 
-        throw new Error("dbjfdkjf");
-        res.send("User Data Sent")
-    } catch (err) { 
-        res.status(500).send("some error contact support team")
-    }
-})
-
-
-app.use('/', (err, req, res, next) => {
-    if (err) {
-        // Log your error
-        res.status(500).send('something went wrong')
-    }
-})
-
-
-app.listen(3001, () => {
-    console.log("server is running");
+    })
 
 })
+    .catch((err) => {
+        console.error('DB can not connect');
+
+    })
+
+
+
